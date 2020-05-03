59a6042d6aff76f6370f4d852ba4a9c8d9f43e14
Priyanka Sharma <priyanka@Priyankas-MacBook-Air.local>
2020-05-03 16:38:51 -0700

    Frontend Checkout

---
+++ frontend/src/components/Address/Address.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+import  './Addresses/21GQUGTNRwL.css';
+import './Addresses/51AZ-Jz5kmL._RC_51da3H-4SUL.css,01evdoiemkL.css,01K+Ps1DeEL.css,31pdJv9iSzL.css,01W6EiNzKkL.css,11UGC+GXOPL.css,21LK7jaicML.css,11L58Qpo0GL.css,21kyTi1FabL.css,01ruG+gDPFL.css,01YhS3Cs-hL.css,21GwE3cR-yL.css,019SHZnt8RL.css,01wAWQRgXzL.css,21bWcRJYNIL.css';
+
+class Address extends Component{
+  render(){
+    return(
+    <div class="a-section" style={{minHeight:'75vh'}}>
+    <div className="a-section a-spacing-medium a-text-left address-wide-container-desktop">
+    <div className="a-section a-spacing-medium"><div className="a-subheader a-breadcrumb a-spacing-small">
+        <ul className="a-nostyle a-horizontal">
+          <li>
+            <span className="a-list-item">
+              <a className="a-spacing-large a-link-normal" href="/gp/css/homepage.html?ref=ya_address_book_address_book_to_your_account_breadcrumb">                        <span> Your Account </span>
+              </a>              </span>
+          </li>
+          <li className="a-breadcrumb-divider">›</li>
+          <li>
+            <span className="a-list-item">
+              <span className="a-color-state"> Your Addresses </span>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <h1 className="a-spacing-medium">Your Addresses</h1>
+    <div className="a-section a-spacing-double-large"><div className="a-row a-spacing-micro">
+        <div className="a-column a-span4 a-spacing-none a-spacing-top-mini address-column">
+          <a id="ya-myab-address-add-link" className="a-link-normal add-new-address-button" href="/a/addresses/add?ref=ya_address_book_add_button"><div className="a-box first-desktop-address-tile"><div className="a-box-inner a-padding-extra-large"><div id="ya-myab-plus-address-icon" className="a-section a-spacing-none address-plus-icon aok-inline-block" /><h2 className="a-color-tertiary">Add Address</h2></div></div></a></div>
+        <div className="a-column a-span4 a-spacing-none a-spacing-top-mini address-column">
+          <div id="ya-myab-display-address-block-0" className="a-box a-spacing-none normal-desktop-address-tile"><div className="a-box-inner a-padding-none">
+              <div className="a-section a-spacing-none default-section"><span className="a-size-small a-color-secondary default-line-item">                Default: &nbsp;
+                </span>
+                <div id="ya-myab-default-shipping-address-icon" className="a-section a-spacing-none amazon-logo aok-inline-block" />
+                <span className="a-size-small a-color-secondary default-line-item">                    &nbsp; | &nbsp;
+                </span>
+                <div id="ya-myab-fresh-address-icon" className="a-section a-spacing-none fresh-logo aok-inline-block" />
+              </div>
+              <div className="a-section address-section-with-default">
+                <div className="a-row a-spacing-small">                <ul className="a-unordered-list a-nostyle a-vertical"><li><span className="a-list-item"><h5 id="address-ui-widgets-FullName" className="id-addr-ux-search-text  a-text-bold">MUKUL SHARMA</h5></span></li><li><span className="a-list-item"><span id="address-ui-widgets-AddressLineOne" className="id-addr-ux-search-text">MUKUL SHARMA 190 RYLAND ST</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-AddressLineTwo" className="id-addr-ux-search-text">APT 4122</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-CityStatePostalCode" className="id-addr-ux-search-text">SAN JOSE, CA 95110-3907</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-Country" className="id-addr-ux-search-text">United States</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-PhoneNumber" className="id-addr-ux-search-text">Phone number: ‪6466399420‬</span></span></li></ul>
+                  <div className="a-row"><span className="a-declarative" data-action="a-modal" data-a-modal="{&quot;cache&quot;:false,&quot;footer&quot;:&quot;\u003cspan class=\&quot;a-declarative\&quot; data-action=\&quot;save-delivery-profile\&quot; data-save-delivery-profile=\&quot;{&quot;modalName&quot;:&quot;cdp-modal-0&quot;}\&quot;>\u003cspan class=\&quot;a-button a-button-primary\&quot;>\u003cspan class=\&quot;a-button-inner\&quot;>\u003cinput class=\&quot;a-button-input\&quot; type=\&quot;submit\&quot;/>\u003cspan class=\&quot;a-button-text\&quot; aria-hidden=\&quot;true\&quot;>Save instructions\u003c\/span>\u003c\/span>\u003c\/span>\u003c\/span>&quot;,&quot;name&quot;:&quot;cdp-modal-0&quot;,&quot;header&quot;:&quot;Add delivery instructions&quot;,&quot;popoverLabel&quot;:&quot;Add delivery instructions&quot;,&quot;url&quot;:&quot;/ma/deliveryProfile?ingressPointType=MYAB&ref=ya_address_book&addressId=ASA5TNAPNMA667K5MEHUG12UYF4M7WL54A3845LW7M4FYUNPXTQ2EIA2OXHKUI6F&cdpViewId=cdp-modal-0&quot;}"><a className="a-link-normal" href="#">            Add delivery instructions
+                      </a></span></div></div>
+              </div></div></div>
+          <div id="ya-myab-edit-address-desktop-row-0" className="a-row edit-address-desktop-link">
+            <span className="a-declarative" data-action="a-modal" data-a-modal="{&quot;footer&quot;:&quot;\u003cdiv class=\&quot;a-row a-spacing-none\&quot;>\u003cdiv class=\&quot;a-column a-span12 a-text-right a-span-last\&quot;>\u003cspan id=\&quot;editAddressModal-0-cancel-btn\&quot; class=\&quot;a-button a-button-span4 a-button-primary\&quot;>\u003cspan class=\&quot;a-button-inner\&quot;>\u003cbutton id=\&quot;editAddressModal-0-cancel-btn-announce\&quot; class=\&quot;a-button-text\&quot; type=\&quot;button\&quot;>Cancel\u003c\/button>\u003c\/span>\u003c\/span>\u003c\/div>\u003c\/div>&quot;,&quot;name&quot;:&quot;editAddressModal-0&quot;,&quot;width&quot;:&quot;400&quot;,&quot;header&quot;:&quot;Edit failed&quot;}"><a id="ya-myab-address-edit-btn-0" className="a-link-normal edit-link" href="#">Edit</a> &nbsp; | &nbsp;
+            </span>
+            <div className="a-popover-preload" id="a-popover-editAddressModal-0"><div className="a-section">
+                <div className="a-row a-spacing-small">      <ul className="a-unordered-list a-nostyle a-vertical"><li><span className="a-list-item"><h5 id="address-ui-widgets-FullName" className="id-addr-ux-search-text  a-text-bold">MUKUL SHARMA</h5></span></li><li><span className="a-list-item"><span id="address-ui-widgets-AddressLineOne" className="id-addr-ux-search-text">MUKUL SHARMA 190 RYLAND ST</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-AddressLineTwo" className="id-addr-ux-search-text">APT 4122</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-CityStatePostalCode" className="id-addr-ux-search-text">SAN JOSE, CA 95110-3907</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-Country" className="id-addr-ux-search-text">United States</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-PhoneNumber" className="id-addr-ux-search-text">Phone number: ‪6466399420‬</span></span></li></ul>
+                </div>
+                <div className="a-row a-spacing-none"><hr className="a-spacing-top-small a-divider-normal" /></div><div className="a-row a-spacing-small">        <div id="address-ui-widgets-arf-edit-error" className="a-box a-alert-inline a-alert-inline-error" aria-live="assertive" role="alert"><div className="a-box-inner a-alert-container"><i className="a-icon a-icon-alert" /><div className="a-alert-content"><div className="a-section">This address is used as your residential address for digital purchases. To delete this address, first <a href="https://www.amazon.com/hz/mycd/myx?ref_=myx_address_ux&redirectAnchors=%23%2Fhome%2Fsettings%2Fcountry%3Factivity%3DchangeCOR">set a different residential address for your digital purchases</a></div></div></div></div>
+                </div>
+              </div></div>
+            <span className="a-declarative" data-action="a-modal" data-a-modal="{&quot;footer&quot;:&quot;\u003cdiv class=\&quot;a-row a-spacing-none\&quot;>\u003cdiv class=\&quot;a-column a-span12 a-text-right a-span-last\&quot;>\u003cspan id=\&quot;deleteAddressModal-0-cancel-btn\&quot; class=\&quot;a-button a-button-span4 a-button-primary\&quot;>\u003cspan class=\&quot;a-button-inner\&quot;>\u003cbutton id=\&quot;deleteAddressModal-0-cancel-btn-announce\&quot; class=\&quot;a-button-text\&quot; type=\&quot;button\&quot;>Cancel\u003c\/button>\u003c\/span>\u003c\/span>\u003c\/div>\u003c\/div>&quot;,&quot;name&quot;:&quot;deleteAddressModal-0&quot;,&quot;width&quot;:&quot;400&quot;,&quot;header&quot;:&quot;Removal failed&quot;}"><a id="ya-myab-address-delete-btn-0" className="a-link-normal delete-link" href="#">Remove</a></span>
+            <div className="a-popover-preload" id="a-popover-deleteAddressModal-0"><div className="a-section">
+                <div className="a-row a-spacing-small">      <ul className="a-unordered-list a-nostyle a-vertical"><li><span className="a-list-item"><h5 id="address-ui-widgets-FullName" className="id-addr-ux-search-text  a-text-bold">MUKUL SHARMA</h5></span></li><li><span className="a-list-item"><span id="address-ui-widgets-AddressLineOne" className="id-addr-ux-search-text">MUKUL SHARMA 190 RYLAND ST</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-AddressLineTwo" className="id-addr-ux-search-text">APT 4122</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-CityStatePostalCode" className="id-addr-ux-search-text">SAN JOSE, CA 95110-3907</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-Country" className="id-addr-ux-search-text">United States</span></span></li><li><span className="a-list-item"><span id="address-ui-widgets-PhoneNumber" className="id-addr-ux-search-text">Phone number: ‪6466399420‬</span></span></li></ul>
+                </div>
+                <div className="a-row a-spacing-none"><hr className="a-spacing-top-small a-divider-normal" /></div><div className="a-row a-spacing-small">        <div id="address-ui-widgets-arf-deletion-error" className="a-box a-alert-inline a-alert-inline-error" aria-live="assertive" role="alert"><div className="a-box-inner a-alert-container"><i className="a-icon a-icon-alert" /><div className="a-alert-content"><div className="a-section">This address is used as your residential address for digital purchases. To delete this address, first <a href="https://www.amazon.com/hz/mycd/myx?ref_=myx_address_ux&redirectAnchors=%23%2Fhome%2Fsettings%2Fcountry%3Factivity%3DchangeCOR">set a different residential address for your digital purchases</a></div></div></div></div>
+                </div>
+              </div></div>
+          </div>
+        </div></div></div>
+      </div> 
+      </div> 
+    )
+  }
+}
+
+export default Address;
---
+++ frontend/src/components/Payment/Your Amazon Wallet_files/01JJeqwU1nL.js
@@ -0,0 +1,1 @@
+(function(){var a=PaymentsPortal2.modules.define;a("components/PayAtStoreComponent",["components/SimplePaymentOptionListItemComponent"],function(a){return a.extend({_getContainerElement:function(){return this.getDOMElement("pasContainer")}})})})();
