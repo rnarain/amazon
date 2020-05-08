1a285449a9539251add783af7b764e1884b1e734
Kapil Mulchandani <kapilmulchandani2012@gmail.com>
2020-05-08 00:21:20 -0700

    deployment changes

--- frontend/src/webConfig.js
+++ frontend/src/webConfig.js
@@ -1,3 +1,4 @@
-const backendServer = "http://localhost:3001";
+// const backendServer = "http://localhost:3001";
+const backendServer = "54.144.184.200";
 const frontendServer = "http://localhost:3000";
 export {backendServer, frontendServer};
